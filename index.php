776ce61ae931305c836a35b58439e3f43c7c6858
hocntph22459 <hocntph22459@fpt.edu.vn>
2022-11-10 09:47:57 +0700

    them logo

--- index.php
+++ index.php
@@ -5,8 +5,13 @@ $url = isset($_GET['url']) ? $_GET['url'] : '';
 // echo $url;
 switch ($url) {
     case '':
+        // trang chủ
     case 'home':
         include "./views/home.php";
+        break;
+        // trang sản phẩm
+    case 'san-pham':
+        include "./views/product.php";
         break;
         //  đăng nhập
     case 'login-khach-hang':
--- views/signin.php
+++ views/signin.php
@@ -20,7 +20,7 @@
 <body>
     <div class="container">
         <nav class="navbar navbar-expand-lg bg-light">
-            <a class="navbar-brand" href="http://localhost/da1/?url=home"><img src="./views/src/image/trangchu/logofoodbry.png" alt="" width="120px"></a>
+            <a class="navbar-brand" href="http://localhost/da1/?url=home"><img src="./views/src/image/logo/logo2.PNG" alt="" width="120px"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
