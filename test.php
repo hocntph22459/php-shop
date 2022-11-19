1d59c63e0598db6e8b42766c62d56c927ef5410c
Vanlq162003 <113500569+Vanlq162003@users.noreply.github.com>
2022-11-19 08:31:19 +0700

    thêm file sửa login

---
+++ test.php
@@ -0,0 +1,35 @@
+<?php
+function login_khachhang()
+{
+    if (isset($_POST['btn'])) {
+        $email = $_POST['email'];
+        $matkhau = $_POST['matkhau'];
+
+        $sql = "select * from users where email = '$email'";
+
+        $user = pdo_query_one($sql);
+
+
+        //KIỂM TRA user
+        if ($user) {
+            //kiểm tra matkhau
+            if ($user['password'] == $matkhau) {
+                $_SESSION['user'] = $user;
+                // $_SESSION['user']['email'] = $user['email'];
+                // $_SESSION['user']['role'] = $user['role'];
+                echo "<pre>";
+                var_dump($_SESSION['user']['email']);
+                die;
+                header("location:http://localhost/da1/?url=home");
+                exit;
+            } else {
+                $error = "tài khoản hoặc mật khẩu không chính xác";
+            }
+        } else {
+            $error = "tài khoản hoặc mật khẩu không chính xác";
+        }
+    }
+    include "./views/login.php";
+}
+
+?>
